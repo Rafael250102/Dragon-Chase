a66661a624d13f14edd91838f642fa251599f1ef
Rafael Queiroz <rqueiroz488@gmail.com>
2025-03-15 10:48:13 -0300

    Implementation of Level 2

--- code/Const.py
+++ code/Const.py
@@ -16,6 +16,13 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Level2Bg5': 5,
+    'Level2Bg6': 6,
     'Player1' : 3,
     'Player1Shot': 2,
     'Player2' : 3,
@@ -34,12 +41,12 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
-    #'Level2Bg0': 999,
-    #'Level2Bg1': 999,
-    #'Level2Bg2': 999,
-    #'Level2Bg3': 999,
-    #'Level2Bg4': 999,
-    #'Level2Bg5': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Level2Bg5': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -58,12 +65,12 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-    #'Level2Bg0': 0,
-    #'Level2Bg1': 0,
-    #'Level2Bg2': 0,
-    #'Level2Bg3': 0,
-    #'Level2Bg4': 0,
-    #'Level2Bg5': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -82,12 +89,12 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-    #'Level2Bg0': 0,
-    #'Level2Bg1': 0,
-    #'Level2Bg2': 0,
-    #'Level2Bg3': 0,
-    #'Level2Bg4': 0,
-    #'Level2Bg5': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -107,6 +114,7 @@ ENTITY_SHOT_DELAY = {
 
 EVENT_ENEMY = pygame.USEREVENT + 1
 
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 
 #M
@@ -130,6 +138,10 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 
 #S
 SPAWN_TIME = 4000
+
+#T
+TIMEOUT_STEP = 100 #100ms
+TIMEOUT_LEVEL = 20000 #20s
 
 
 #W
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -13,9 +13,15 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range (7):
+                for i in range (7): # Level 1 background images number
                     list_bg.append(Background(f'Level1Bg{i}', (0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg': # Level 2 background images number
+                list_bg = []
+                for i in range(6):
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
