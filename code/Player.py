a4517af5e47c0f4c1f05ebe99839c29728492ac9
Rafael Queiroz <rqueiroz488@gmail.com>
2025-03-26 10:24:49 -0300

    Creating function pause Creating menu pause

--- code/Player.py
+++ code/Player.py
@@ -4,6 +4,8 @@ from code.Entity import Entity
 from code.PlayerShot import PlayerShot
 from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
     PLAYER_KEY_RIGHT, PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY, ENTITY_HEALTH
+from pygame.examples.sprite_texture import running
+
 
 class Player(Entity):
 
@@ -21,6 +23,8 @@ class Player(Entity):
             self.rect.centerx -= ENTITY_SPEED[self.name]
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.bottom < WIN_WIDTH:
             self.rect.centerx += ENTITY_SPEED[self.name]
+        if pressed_key[pygame.K_ESCAPE]: # Menu pause
+            self.pause_game()
         pass
 
     def shoot(self):
@@ -30,3 +34,11 @@ class Player(Entity):
             pressed_key = pygame.key.get_pressed()
             if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
                 return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+
+    def pause_game(self): # Menu Pause
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        is_paused = False
